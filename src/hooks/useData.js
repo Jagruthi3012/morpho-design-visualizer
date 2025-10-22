676e3ab85afb1b2ee46c07f1ba6c6ec2283d0e91
Jagruthi3012 <jkelaval@buffalo.edu>
2025-10-22 14:47:30 -0400

    Changes supporting plugin integration

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,22 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// const useData = () => {
-//   const [data, setData] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     fetch("/data.json")
-//       .then((res) => res.json())
-//       .then((json) => {
-//         setData(json);
-//         setLoading(false);
-//       });
-//   }, []);
-
-//   return { data, loading };
-// };
-
-// export default useData;
 import { useEffect, useState } from "react";
 import dataJson from "../assets/data.json"; 
 
